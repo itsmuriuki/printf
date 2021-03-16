a7917f577449a6a4d8aa6969e106e9d45a6cda41
itsmuriuki <grdmuriuki@gmail.com>
2021-03-16 20:56:39 +0000

    betty compliant iterator

--- printfiterator.c
+++ printfiterator.c
@@ -16,76 +16,76 @@
  */
 int formats(const char *form, specifiers *spec, va_list args, char *b, int *bi)
 {
-  int fi, si, sti = 0, totalcount = 0;
-  char *str;
+int fi, si, sti = 0, totalcount = 0;
+char *str;
 
-  for (fi = 0; form[fi] != '\0'; fi++)
-    {
-      if (form[fi] == '%' && form[fi + 1] != '\0')
-	{
-	  fi++;
-	  while (form[fi] == ' ' && form != '\0')
-	    fi++;
+for (fi = 0; form[fi] != '\0'; fi++)
+{
+if (form[fi] == '%' && form[fi + 1] != '\0')
+{
+fi++;
+while (form[fi] == ' ' && form != '\0')
+fi++;
 
-	  for (si = 0; spec[si].s != NULL; si++)
-	    if (*(spec[si].s) == form[fi])
-	      {
-		str = (spec[si].printspec)(args);
-		if (str == NULL)
-		  return (-1);
-		sti = 0;
-		while (str[sti] != '\0')
-		  {
-		    b[*bi] = str[sti];
-		    if (*bi == 1024)
-		      {
-			totalcount += printfbufferoverflow(b);
-			*bi = 0;
-		      }
-		    else
-		      {
-			sti++;
-			(*bi)++;
-		      }
-		  }
-		free(str);
-		break;
-	      }
-	  if (spec[si].s == NULL)
-	    {
-	      b[*bi] = '%';
-	      if (*bi == 1024)
-		{
-		  totalcount += printfbufferoverflow(b);
-		  *bi = 0;
-		}
-	      else
-		(*bi)++;
-	      if (form[fi] != '%')
-		{
-		  b[*bi] = form[fi];
-		  if (*bi == 1024)
-		    {
-		      totalcount += printfbufferoverflow(b);
-		      *bi = 0;
-		    }
-		  else
-		    (*bi)++;
-		}
-	    }
-	}
-      else
-	{
-	  b[*bi] = form[fi];
-	  if (*bi == 1024)
-	    {
-	      totalcount += printfbufferoverflow(b);
-	      *bi = 0;
-	    }
-	  else
-	    (*bi)++;
-	}
-    }
-  totalcount = totalcount + *bi;
-  return (totalcount);
+for (si = 0; spec[si].s != NULL; si++)
+if (*(spec[si].s) == form[fi])
+{
+str = (spec[si].printspec)(args);
+if (str == NULL)
+return (-1);
+sti = 0;
+while (str[sti] != '\0')
+{
+b[*bi] = str[sti];
+if (*bi == 1024)
+{
+totalcount += printfbufferoverflow(b);
+*bi = 0;
+}
+else
+{
+sti++;
+(*bi)++;
+}
+}
+free(str);
+break;
+}
+if (spec[si].s == NULL)
+{
+b[*bi] = '%';
+if (*bi == 1024)
+{
+totalcount += printfbufferoverflow(b);
+*bi = 0;
+}
+else
+(*bi)++;
+if (form[fi] != '%')
+{
+b[*bi] = form[fi];
+if (*bi == 1024)
+{
+totalcount += printfbufferoverflow(b);
+*bi = 0;
+}
+else
+(*bi)++;
+}
+}
+}
+else
+{
+b[*bi] = form[fi];
+if (*bi == 1024)
+{
+totalcount += printfbufferoverflow(b);
+*bi = 0;
+}
+else
+(*bi)++;
+}
+}
+totalcount = totalcount + *bi;
+return (totalcount);
 }
