5333884c1450d2da8effff37d25a15af8d3bb623
theurikenneth <theurikenneth@gmail.com>
2021-03-16 20:13:38 +0000

    collaboration

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
+ * main - Entry point for printf
  *
  * Return: Always 0
  */
@@ -12,33 +12,29 @@ int main(void)
   int len;
   int len2;
   unsigned int ui;
-  void *addr;
 
   len = _printf("Let's try to printf a simple sentence.\n");
   len2 = printf("Let's try to printf a simple sentence.\n");
   ui = (unsigned int)INT_MAX + 1024;
-  addr = (void *)0x7ffe637541f0;
-  _printf("Length:[%d, %i]\n", len, len);
-  printf("Length:[%d, %i]\n", len2, len2);
-  _printf("Negative:[%d]\n", -762534);
-  printf("Negative:[%d]\n", -762534);
-  _printf("Unsigned:[%u]\n", ui);
-  printf("Unsigned:[%u]\n", ui);
-  _printf("Unsigned octal:[%o]\n", ui);
-  printf("Unsigned octal:[%o]\n", ui);
-  _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-  printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+  _printf("Let's try to do this %%s\n");
+  _printf("Character:[%c]\n", 'H');
+  printf("Character:[%c]\n", 'H');
   _printf("Character:[%c]\n", 'H');
   printf("Character:[%c]\n", 'H');
   _printf("String:[%s]\n", "I am a string !");
   printf("String:[%s]\n", "I am a string !");
-  _printf("Address:[%p]\n", addr);
-  printf("Address:[%p]\n", addr);
-  len = _printf("Percent:[%%]\n");
-  len2 = printf("Percent:[%%]\n");
+  _printf("Negative:[%d]\n", -762534);
+  printf("Negative:[%d]\n", -762534);
   _printf("Len:[%d]\n", len);
   printf("Len:[%d]\n", len2);
-  _printf("Unknown:[%r]\n");
-  printf("Unknown:[%r]\n");
+  _printf("[%i]\n", 0x10);
+  _printf("[%b]\n", 13);
+  _printf("[%u]\n", -500);
+  printf("[%u]\n", -500);
+  _printf("[%u]\n", 500);
+  _printf("Unsigned octal:[%o]\n", ui);
+  printf("Unsigned octal:[%o]\n", ui);
+  _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+  printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
   return (0);
 }
